b807fb84189516b42a30f776969512c7efdd2c76
Solly Ross <sross@redhat.com>
2018-08-20 17:03:44 -0400

    Add metrics-server as a dependency
    metrics-server contains the machinery for serving the resource metrics API,
    so we pull in the generic parts (thanks to dep we only get the generic
    parts, though).

--- vendor/github.com/kubernetes-incubator/custom-metrics-apiserver/pkg/cmd/builder.go
+++ vendor/github.com/kubernetes-incubator/custom-metrics-apiserver/pkg/cmd/builder.go
@@ -25,14 +25,29 @@ import (
 	apimeta "k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
+	"github.com/kubernetes-incubator/custom-metrics-apiserver/pkg/apiserver"
 	"github.com/kubernetes-incubator/custom-metrics-apiserver/pkg/cmd/server"
 	"github.com/kubernetes-incubator/custom-metrics-apiserver/pkg/dynamicmapper"
 	"github.com/kubernetes-incubator/custom-metrics-apiserver/pkg/provider"
 )
 
+// AdapterBase provides a base set of functionality for any custom metrics adapter.
+// Embed it in a struct containing your options, then:
+//
+// - Use Flags() to add flags, then call Flags().Parse(os.Argv)
+// - Use DynamicClient and RESTMapper to fetch handles to common utilities
+// - Use WithCustomMetrics(provider) and WithExternalMetrics(provider) to install metrics providers
+// - Use Run(stopChannel) to start the server
+//
+// All methods on this struct are idempotent except for Run -- they'll perform any
+// initialization on the first call, then return the existing object on later calls.
+// Methods on this struct are not safe to call from multiple goroutines without
+// external synchronization.
 type AdapterBase struct {
 	*server.CustomMetricsAdapterServerOptions
 
@@ -48,7 +63,7 @@ type AdapterBase struct {
 
 	// FlagSet is the flagset to add flags to.
 	// It defaults to the normal CommandLine flags
-	// if not explicitly set
+	// if not explicitly set.
 	FlagSet *pflag.FlagSet
 
 	// flagOnce controls initialization of the flags.
@@ -58,20 +73,23 @@ type AdapterBase struct {
 	discoveryClient discovery.DiscoveryInterface
 	restMapper      apimeta.RESTMapper
 	dynamicClient   dynamic.Interface
+	informers       informers.SharedInformerFactory
+
+	config *apiserver.Config
+	server *apiserver.CustomMetricsAdapterServer
 
 	cmProvider provider.CustomMetricsProvider
 	emProvider provider.ExternalMetricsProvider
 }
 
 // InstallFlags installs the minimum required set of flags into the flagset.
-// It's safe to call multiple times.
 func (b *AdapterBase) InstallFlags() {
 	b.initFlagSet()
 	b.flagOnce.Do(func() {
 		if b.CustomMetricsAdapterServerOptions == nil {
 			b.CustomMetricsAdapterServerOptions = server.NewCustomMetricsAdapterServerOptions()
 		}
-		
+
 		b.SecureServing.AddFlags(b.FlagSet)
 		b.Authentication.AddFlags(b.FlagSet)
 		b.Authorization.AddFlags(b.FlagSet)
@@ -85,6 +103,7 @@ func (b *AdapterBase) InstallFlags() {
 	})
 }
 
+// initFlagSet populates the flagset to the CommandLine flags if it's not already set.
 func (b *AdapterBase) initFlagSet() {
 	if b.FlagSet == nil {
 		// default to the normal commandline flags
@@ -102,6 +121,10 @@ func (b *AdapterBase) Flags() *pflag.FlagSet {
 	return b.FlagSet
 }
 
+// ClientConfig returns the REST client configuration used to construct
+// clients for the clients and RESTMapper, and may be used for other
+// purposes as well.  If you need to mutate it, be sure to copy it with
+// rest.CopyConfig first.
 func (b *AdapterBase) ClientConfig() (*rest.Config, error) {
 	if b.clientConfig == nil {
 		var clientConfig *rest.Config
@@ -122,6 +145,8 @@ func (b *AdapterBase) ClientConfig() (*rest.Config, error) {
 	return b.clientConfig, nil
 }
 
+// DiscoveryClient returns a DiscoveryInterface suitable to for discovering resources
+// available on the cluster.
 func (b *AdapterBase) DiscoveryClient() (discovery.DiscoveryInterface, error) {
 	if b.discoveryClient == nil {
 		clientConfig, err := b.ClientConfig()
@@ -137,6 +162,8 @@ func (b *AdapterBase) DiscoveryClient() (discovery.DiscoveryInterface, error) {
 	return b.discoveryClient, nil
 }
 
+// RESTMapper returns a RESTMapper dynamically populated with discovery information.
+// The discovery information will be periodically repopulated according to DiscoveryInterval.
 func (b *AdapterBase) RESTMapper() (apimeta.RESTMapper, error) {
 	if b.restMapper == nil {
 		discoveryClient, err := b.DiscoveryClient()
@@ -155,6 +182,8 @@ func (b *AdapterBase) RESTMapper() (apimeta.RESTMapper, error) {
 	return b.restMapper, nil
 }
 
+// DynamicClient returns a dynamic Kubernetes client capable of listing and fetching
+// any resources on the cluster.
 func (b *AdapterBase) DynamicClient() (dynamic.Interface, error) {
 	if b.dynamicClient == nil {
 		clientConfig, err := b.ClientConfig()
@@ -170,29 +199,85 @@ func (b *AdapterBase) DynamicClient() (dynamic.Interface, error) {
 	return b.dynamicClient, nil
 }
 
+// WithCustomMetrics populates the custom metrics provider for this adapter.
 func (b *AdapterBase) WithCustomMetrics(p provider.CustomMetricsProvider) {
 	b.cmProvider = p
 }
 
+// WithExternalMetrics populates the external metrics provider for this adapter.
 func (b *AdapterBase) WithExternalMetrics(p provider.ExternalMetricsProvider) {
 	b.emProvider = p
 }
 
+// Config fetches the configuration used to ulitmately create the custom metrics adapter's
+// API server.  While this method is idempotent, it does "cement" values of some of the other
+// fields, so make sure to only call it just before `Server` or `Run`.
+// Normal users should not need to call this method -- it's for advanced use cases.
+func (b *AdapterBase) Config() (*apiserver.Config, error) {
+	if b.config == nil {
+		b.InstallFlags() // just to be sure
+
+		config, err := b.CustomMetricsAdapterServerOptions.Config()
+		if err != nil {
+			return nil, err
+		}
+		b.config = config
+	}
+
+	return b.config, nil
+}
+
+// Server fetches API server object used to ulitmately run the custom metrics adapter.
+// While this method is idempotent, it does "cement" values of some of the other
+// fields, so make sure to only call it just before `Run`.
+// Normal users should not need to call this method -- it's for advanced use cases.
+func (b *AdapterBase) Server() (*apiserver.CustomMetricsAdapterServer, error) {
+	if b.server == nil {
+		config, err := b.Config()
+		if err != nil {
+			return nil, err
+		}
+
+		if b.Name == "" {
+			b.Name = "custom-metrics-adapter"
+		}
+
+		// we add in the informers if they're not nil, but we don't try and
+		// construct them if the user didn't ask for them
+		server, err := config.Complete(b.informers).New(b.Name, b.cmProvider, b.emProvider)
+		if err != nil {
+			return nil, err
+		}
+		b.server = server
+	}
+
+	return b.server, nil
+}
+
+// Informers returns a SharedInformerFactory for constructing new informers.
+// The informers will be automatically started as part of starting the adapter.
+func (b *AdapterBase) Informers() (informers.SharedInformerFactory, error) {
+	if b.informers == nil {
+		clientConfig, err := b.ClientConfig()
+		if err != nil {
+			return nil, err
+		}
+		kubeClient, err := kubernetes.NewForConfig(clientConfig)
+		if err != nil {
+			return nil, err
+		}
+		b.informers = informers.NewSharedInformerFactory(kubeClient, 0)
+	}
+
+	return b.informers, nil
+}
+
+// Run runs this custom metrics adapter until the given stop channel is closed.
 func (b *AdapterBase) Run(stopCh <-chan struct{}) error {
-	b.InstallFlags()  // just to be sure
-
-	if b.Name == "" {
-		b.Name = "custom-metrics-adapter"
-	}
-
-	config, err := b.Config()
+	server, err := b.Server()
 	if err != nil {
 		return err
 	}
 
-	server, err := config.Complete().New(b.Name, b.cmProvider, b.emProvider)
-	if err != nil {
-		return err
-	}
 	return server.GenericAPIServer.PrepareRun().Run(stopCh)
 }
